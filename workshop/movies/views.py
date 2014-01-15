4dca6b2c9d36ec4e5baae202bab87b294cf31511
sivaa <siva@sivaa.in>
2014-01-15 18:41:42 +0530

    validation added

--- workshop/movies/views.py
+++ workshop/movies/views.py
@@ -1,7 +1,8 @@
+from django.db.utils import IntegrityError
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 from movies.models import Movie
-from django.http.response import HttpResponse
 
 
 def get_movies():
@@ -13,16 +14,27 @@ def movies(request):
         return render(request, "movies.html", {'movies': get_movies()})
     if request.method == 'POST':
         name = request.POST.get('name', None)
-        Movie.objects.create(name=name)
-        message = "Movie '%s' added successfully!" % name
+        if len(name.strip()) == 0:
+            message = "Please enter a movie  name"
+        elif len(name.strip()) < 3:
+            message = "Not enough words!"
+        else:
+            try:
+                Movie.objects.create(name=name)
+                message = "Movie '%s' added successfully!" % name
+            except IntegrityError:
+                message = "Movie '%s' already exists!" % name
         return render(request, "movies.html", {'message': message, 'movies': get_movies()})
     return HttpResponse("Invalid Request")
 
 def movies_remove(request):
     if request.method == 'GET':
         movie_id = request.GET.get('id', None)
-        movie = Movie.objects.get(pk=movie_id)
-        movie.delete()
-        message = "Movie '%s' deleted successfully!" % movie.name
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+            movie.delete()
+            message = "Movie '%s' deleted successfully!" % movie.name
+        except:
+            message = "Invalid Movie. Delete Failed!"
         return render(request, "movies.html", {'message': message, 'movies': get_movies()})
     return HttpResponse("Invalid Request")
