b9930066d85c8847f4685ea97c1e9a2ba89f638b
sivaa <siva@sivaa.in>
2014-01-15 17:08:53 +0530

    updated the placeholders

--- workshop/workshop/urls.py
+++ workshop/workshop/urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
-from hello.views import hello1, hello2, hello3
-
+# from movies.views import movies
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^hello1/$', hello1),
-    url(r'^hello2/$', hello2),
-    url(r'^hello3/$', hello3),
+#    url(r'^movies/$', movies),
 
     url(r'^admin/', include(admin.site.urls)),
 )
