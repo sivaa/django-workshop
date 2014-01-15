1063f9dd7d1a8a46ed869dfb88afae855099f165
sivaa <siva@sivaa.in>
2014-01-15 18:07:39 +0530

    movie listing with add form

--- workshop/movies/views.py
+++ workshop/movies/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 from movies.models import Movie
 from django.http.response import HttpResponse
 
+
+def get_movies():
+    movies = Movie.objects.all()
+    return movies
+
 def movies(request):
     if request.method == 'GET':
-        return render(request, "movies.html")
+        return render(request, "movies.html", {'movies': get_movies()})
     if request.method == 'POST':
         name = request.POST.get('name', None)
         Movie.objects.create(name=name)
         message = "Movie '%s' added successfully!" % name
-        return render(request, "movies.html", {'message': message})
+        return render(request, "movies.html", {'message': message, 'movies': get_movies()})
     return HttpResponse("Invalid Request")
