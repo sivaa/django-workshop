fc56f62e85a7f3840ac09960a62321d943153f36
sivaa <siva@sivaa.in>
2014-01-15 16:41:35 +0530

    added hello world examples

--- workshop/hello/views.py
+++ workshop/hello/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("Hello World!")
+
+def hello1(request):
+    return HttpResponse("<h1> Hello World!</h1>")
+
+def hello2(request):
+    return HttpResponse("Hello World! at %s" % datetime.now())
+
+def hello3(request):
+    name = request.GET.get('name', "Django")
+    return HttpResponse("Hello World! %s" % name)
--- workshop/workshop/urls.py
+++ workshop/workshop/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from hello.views import hello, hello1, hello2, hello3
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'workshop.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^hello/$', hello),
+    url(r'^hello1/$', hello1),
+    url(r'^hello2/$', hello2),
+    url(r'^hello3/$', hello3),
 
     url(r'^admin/', include(admin.site.urls)),
 )
