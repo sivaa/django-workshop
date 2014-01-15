8558f717d0b10582ff21578b272fa18d90dcc4b9
sivaa <siva@sivaa.in>
2014-01-15 17:48:31 +0530

    implemented movie add logic

--- workshop/movies/views.py
+++ workshop/movies/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from movies.models import Movie
+from django.http.response import HttpResponse
+
 def movies(request):
+    if request.method == 'GET':
         return render(request, "movies.html")
+    if request.method == 'POST':
+        name = request.POST.get('name', None)
+        Movie.objects.create(name=name)
+        return HttpResponse("Movie '%s' added successfully!" % name)
+    return HttpResponse("Invalid Request")
