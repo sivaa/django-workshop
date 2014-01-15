8402144ac14035bd3450f08ee124545f0746e20b
sivaa <siva@sivaa.in>
2014-01-15 16:52:00 +0530

    updated the place holders

--- workshop/hello/views.py
+++ workshop/hello/views.py
@@ -1,15 +1,13 @@
-from django.http.response import HttpResponse
-from datetime import datetime
-
-def hello(request):
-    return HttpResponse("Hello World!")
+from django.shortcuts import render
 
 def hello1(request):
-    return HttpResponse("<h1> Hello World!</h1>")
+    # Just a Hello World
+    pass
 
 def hello2(request):
-    return HttpResponse("Hello World! at %s" % datetime.now())
+    # Hello World with Current Time
+    pass
 
 def hello3(request):
-    name = request.GET.get('name', "Django")
-    return HttpResponse("Hello World! %s" % name)
+    # Hello World with Query String
+    pass
--- workshop/workshop/urls.py
+++ workshop/workshop/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from hello.views import hello, hello1, hello2, hello3
+# from hello.views import hello1, hello2, hello3
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'workshop.views.home', name='home'),
 
-    url(r'^hello/$', hello),
-    url(r'^hello1/$', hello1),
-    url(r'^hello2/$', hello2),
-    url(r'^hello3/$', hello3),
+#     url(r'^hello1/$', hello1),
+#     url(r'^hello2/$', hello2),
+#     url(r'^hello3/$', hello3),
 
     url(r'^admin/', include(admin.site.urls)),
 )
