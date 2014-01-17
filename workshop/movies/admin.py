f446aecdf6bae4debf0a5b3eb87867a02803010a
sivaa <siva@sivaa.in>
2014-01-17 18:37:10 +0530

    enhanced admin

--- workshop/movies/admin.py
+++ workshop/movies/admin.py
@@ -4,6 +4,8 @@ from movies.models import Movie
 from movies.forms import MovieForm
 
 class MovieAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    search_fields = ('name',)
     form = MovieForm
 
 admin.site.register(Movie, MovieAdmin)
