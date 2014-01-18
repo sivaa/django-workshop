b2f2b13034da43828afb09f9c9d47eb13bc76a5a
ganeshkumar <ganeshkumarmail@gmail.com>
2014-01-18 10:44:56 +0530

    Bootstrap Template Added

--- workshop/todo/views.py
+++ workshop/todo/views.py
@@ -20,7 +20,7 @@ def tasks(request):
         if 'message' in request.session:
             message = request.session['message']
             del request.session['message']
-        return render(request, "tasks.html", {'tasks': get_tasks(), 'message': message, 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': get_tasks(), 'message': message, 'form': TaskForm(),'page':'All'})
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -87,30 +87,30 @@ def tasks_remove(request, task_id):
 
 def tasks_priority_high(request):
     if request.method == 'GET':
-        return render(request, "tasks.html", {'tasks': Task.objects.filter(priority = 'H'), 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': Task.objects.filter(priority = 'H'), 'form': TaskForm(),'page':'H'})
     return HttpResponse("Invalid Request")
 
 def tasks_priority_low(request):
     if request.method == 'GET':
-        return render(request, "tasks.html", {'tasks': Task.objects.filter(priority = 'L'), 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': Task.objects.filter(priority = 'L'), 'form': TaskForm(),'page':'L'})
     return HttpResponse("Invalid Request")
 
 def tasks_completed(request):
     if request.method == 'GET':
-        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = True), 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = True), 'form': TaskForm(),'page':'C'})
     return HttpResponse("Invalid Request")
 
 def tasks_not_completed(request):
     if request.method == 'GET':
-        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = False), 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = False), 'form': TaskForm(),'page':'NC'})
     return HttpResponse("Invalid Request")
 
 def tasks_overdue(request):
     if request.method == 'GET':
-        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = False, last_date__lt = datetime.date.today()), 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = False, last_date__lt = datetime.date.today()), 'form': TaskForm(),'page':'OD'})
     return HttpResponse("Invalid Request")
 
 def tasks_due_today(request):
     if request.method == 'GET':
-        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = False, last_date = datetime.date.today()), 'form': TaskForm()})
+        return render(request, "tasks.html", {'tasks': Task.objects.filter(done = False, last_date = datetime.date.today()), 'form': TaskForm(),'page':'T'})
     return HttpResponse("Invalid Request")
--- workshop/workshop/settings.py
+++ workshop/workshop/settings.py
@@ -84,3 +84,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),)
+
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
